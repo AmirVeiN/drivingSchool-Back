9037837c48ac4615b3790b88be77c1eb7838c6fe
amirvein <amir.aha73566@gmail.com>
2024-10-27 19:56:52 +0330

    fix serilizer

--- Admin/serializers.py
+++ Admin/serializers.py
@@ -8,6 +8,11 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id','name', 'address', 'codemeli', 'sen','telephone', 'noe_tadris', 'user_type']
 
 class ClassSerializer(serializers.ModelSerializer):
+    morabi_name = serializers.CharField(source='morabi.name', read_only=True)
+
     class Meta:
         model = Class
-        fields = ['id', 'address', 'class_number', 'class_time', 'class_created', 'morabi','noe_tadris']
+        fields = ['id', 'address', 'class_number', 'class_time', 'class_created', 'morabi', 'morabi_name', 'noe_tadris']
+        extra_kwargs = {
+            'morabi': {'write_only': True}
+        }
